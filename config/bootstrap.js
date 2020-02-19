f8eab4aef4c0744f317a4fde8285d59b81c8d12e
Wong Tsz Hei Ernest <lz5663@hotmail.com.hk>
2020-02-20 00:54:42 +0800

    Table edition and bootstrap.js edit password item

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -35,9 +35,9 @@ module.exports.bootstrap = async function() {
     const hash = await sails.bcrypt.hash('123456', saltRounds);
 
     await User.createEach([
-      { username: 'admin1', password: hash, role: 'admin' },
-      { username: 'admin2', password: hash, role: 'admin' },
-      { username: 'station1', password: hash, role: 'stationmgr' }
+      { username: 'admin1', password: '123456', role: 'admin' },
+      { username: 'admin2', password: '123456', role: 'admin' },
+      { username: 'station1', password: '123456', role: 'stationmgr' }
       // etc.
     ]);
   }
