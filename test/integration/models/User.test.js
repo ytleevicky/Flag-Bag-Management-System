34f34ea068a238521c8adc1563f2eaa355a5b7b4
Yui Tung Lee <17228336@life.hkbu.edu.hk>
2020-04-08 20:59:03 +0800

    Modified User.test.js

--- test/integration/models/User.test.js
+++ test/integration/models/User.test.js
@@ -1,38 +1,89 @@
 //  Test the initial data inserted in config/bootstrap.js
-describe('User (model) initial data', function() {
+describe('User (model) initial data', function () {
 
-    describe('Admin user', function() {
-      it('should find User with role "admin"', function (done) {
-        User.find({role:'admin'})
-        .then(function(users) {
-  
-          if (users.length == 0) {
-            return done(new Error(
-              'Should return 1 user with role -- admin '+
-              'But instead, got: no user found'
-            ));
-          }
-          return done();
-        })
-        .catch(done);
-      });
+    describe('Admin user', function () {
+        it('should find User with username "admin1"', function (done) {
+            User.find({ username: 'admin1' })
+                .then(function (users) {
+
+                    if (users.length == 0) {
+                        return done(new Error(
+                            'Should return 1 user with username -- admin1 ' +
+                            'But instead, got: no user found'
+                        ));
+                    }
+                    return done();
+                })
+                .catch(done);
+        });
     });
-  
-    describe('Stationmgr user', function() {
-      it('should find User with role "stationmgr"', function (done) {
-        User.find({role: 'stationmgr'})
-        .then(function(users) {
-  
-          if (users.length == 0) {
-            return done(new Error(
-              'Should return at least 1 user with role -- stationmgr '+
-              'But instead, got: no user found'
-            ));
-          }
-          return done();
-        })
-        .catch(done);
-      });
+
+    describe('Admin user', function () {
+        it('should find User with username "admin2"', function (done) {
+            User.find({ username: 'admin2' })
+                .then(function (users) {
+
+                    if (users.length == 0) {
+                        return done(new Error(
+                            'Should return 1 user with username -- admin2 ' +
+                            'But instead, got: no user found'
+                        ));
+                    }
+                    return done();
+                })
+                .catch(done);
+        });
     });
-  
-  });
+
+    describe('Stationmgr user', function () {
+        it('should find User with username "stationmgr1"', function (done) {
+            User.find({ username: 'stationmgr1' })
+                .then(function (users) {
+
+                    if (users.length == 0) {
+                        return done(new Error(
+                            'Should return 1 user with username -- stationmgr1 ' +
+                            'But instead, got: no user found'
+                        ));
+                    }
+                    return done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe('Stationmgr user', function () {
+        it('should find User with username "stationmgr2"', function (done) {
+            User.find({ username: 'stationmgr2' })
+                .then(function (users) {
+
+                    if (users.length == 0) {
+                        return done(new Error(
+                            'Should return 1 user with username -- stationmgr2 ' +
+                            'But instead, got: no user found'
+                        ));
+                    }
+                    return done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe('Stationmgr user', function () {
+        it('should find User with username "stationmgr3"', function (done) {
+            User.find({ username: 'stationmgr3' })
+                .then(function (users) {
+
+                    if (users.length == 0) {
+                        return done(new Error(
+                            'Should return 1 user with username -- stationmgr3 ' +
+                            'But instead, got: no user found'
+                        ));
+                    }
+                    return done();
+                })
+                .catch(done);
+        });
+    });
+
+});
