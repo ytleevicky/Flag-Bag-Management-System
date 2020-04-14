6aac92f12caacfd2d9c49a7eff592b3c6d2d662f
Wong Tsz Hei Ernest <lz5663@hotmail.com.hk>
2020-04-14 16:25:38 +0800

    Test StationController func

--- test/integration/controllers/StationController.test.js
+++ test/integration/controllers/StationController.test.js
@@ -4,6 +4,7 @@ const Async = require('async');
 describe('StationController', () => {
 
   let cookie;
+  let eventId;
 
   // createStation
   describe(`Policy Check: #createStation() Station[sName]=TSW-S2, Station[numOfSpareBag]=6 and Station[sLocation]=HK && User[username]=stationmgr2 without login`, () => {
@@ -15,7 +16,7 @@ describe('StationController', () => {
     });
   });
 
-  describe(`#createStation() Station[sName]=TSW-S2, Station[numOfSpareBag]=6 and Station[sLocation]=HK && User[username]=stationmgr2 with admin1 login`, () => {
+  describe(`#createStation() Station[sName]=TSW-S3, Station[numOfSpareBag]=6 and Station[sLocation]=HK && User[username]=stationmgr2 with admin1 login`, () => {
     it('should return 200 "Successfully created!"', (done) => {
       Async.series([
         function (cb) {
@@ -29,14 +30,38 @@ describe('StationController', () => {
               cb();
             });
         },
+        function (cb) {
+          Web.findOne({where: {eventName: '齊抗武漢肺炎賣旗活動'} }).then(model => {
+            if (model) {
+              eventId = model.id;
+            }
+            cb();
+          });
+        },
+        function (cb) {
+          supertest(sails.hooks.http.app)
+          .get('/viewitem/'+eventId)
+          .set('Cookie', cookie)
+          .expect(200, cb);
+        },
+        
         // Have BUGS --- Could not find the req.session.eventid
-        // function (cb) {
-        //   supertest(sails.hooks.http.app)
-        //     .post('/addflagstn/')
-        //     .set('Cookie', cookie)
-        //     .send('Station[sName]=TSW-S2&Station[numOfSpareBag]=6&Station[sLocation]=HK&User[username]=stationmgr2&req.session.eventid=1')
-        //     .expect(302, cb);
-        // }
+        function (cb) {
+          supertest(sails.hooks.http.app)
+            .post('/addflagstn/')
+            .set('Cookie', cookie)
+            .set('Accept', 'text/html,application/xhtml+xml,application/xml')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .send('Station[sName]=TSW-S3&Station[numOfSpareBag]=6&Station[sLocation]=HK&User[username]=stationmgr2')
+            .expect(302).then(() => {
+              Station.findOne( {where: { sName: 'TSW-S3' }}).then(model => {
+                if (model) {
+                  return cb();
+                }
+                return cb(new Error('Can\'t find TSW-S3'));
+              });
+            });
+        }
       ], done);
     });
   });
